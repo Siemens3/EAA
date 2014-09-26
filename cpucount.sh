42faf41e83fbe8a93ea72c5531876d3ffe6e29d4
Siemens3 <steo.kenny@hotmail.co.uk>
2014-09-26 15:44:05 +0100

    added more info to cpucount.sh

--- cpucount.sh
+++ cpucount.sh
@@ -1,2 +1,11 @@
- grep 'processor' cpuinfo | wc -l
+#!/bin/bash
+
+# Title :cpucount.sh 
+# Date  : 26/02/14
+# Author  :Stephen kenny
+# Version :1.0
+# Description : counts the number of cpus on guest machine
+# Options  
+
+ grep 'processor' /proc/cpuinfo | wc -l
 
