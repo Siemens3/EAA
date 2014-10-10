b3118b3221a1d1d327223e7cde2e41ed37727a6b
Siemens3 <steo.kenny@hotmail.co.uk>
2014-10-10 14:46:13 +0100

    commiting to EAA depsotiroy

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Title :script.sh 
+# Date  : 10/10/14
+# Author  :Stephen kenny
+# Version :1.0
+# Description : Takes a parameter and prints to the screen. 
+# Options  
+
+echo "Hello $1"
